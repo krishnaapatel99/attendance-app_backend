67fad2b0ff034978cdfbd6b1749d64ea7c939f9e
Krishna Patel <patelkrishu1608@gmail.com>
2026-01-31 13:42:43 +0530

    cors error fixed

--- index.js
+++ index.js
@@ -28,13 +28,16 @@ const allowedOrigins = [
 app.use(cors({
   origin: (origin, callback) => {
     if (!origin) return callback(null, true);
+
     if (allowedOrigins.includes(origin)) {
-      return callback(null, origin); // IMPORTANT
+      return callback(null, true); // ✅ FIX
     }
+
     return callback(new Error("Not allowed by CORS"));
   },
   credentials: true,
 }));
+
 
 
 // Health check endpoint
